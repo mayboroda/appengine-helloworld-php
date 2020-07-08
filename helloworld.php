216ff94b2ff3616e706beb2be20708dc6f367dc1
Dmitriy Mayboroda <dmytromay@wix.com>
2020-07-08 13:14:22 +0300

    Add new colors

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,16 @@
-<?php
-  echo 'Hello, World!';
+<html>
+  <head>
+    <style type="text/css">
+      body {
+        background-color: #85144b
+      }
+
+      h1 {
+        color: maroon
+      }
+    </style>
+  </head>
+  <body>
+    <h1><?php echo 'Hello, World!'; ?></h1>
+  </body>
+</html>
